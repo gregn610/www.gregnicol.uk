fa4732c8815ed7107958bf60444343f3a0a5ef79
gregn <greg@gregnicol.uk>
2021-01-19 08:11:31 +0000

    Feature: Switch DNS to Cloudfront

--- src/terraform/modules/www-site/route53.tf
+++ src/terraform/modules/www-site/route53.tf
@@ -3,14 +3,13 @@ module "records" {
   source = "terraform-aws-modules/route53/aws//modules/records"
 
   zone_id = data.aws_route53_zone.this.zone_id
-
   records = [
     {
-      name = "www"
+      name = local.subdomain
       type = "A"
       alias = {
-        name    = module.www_bucket.this_s3_bucket_website_domain
-        zone_id = module.www_bucket.this_s3_bucket_hosted_zone_id
+        name    = module.cloudfront.this_cloudfront_distribution_domain_name
+        zone_id = module.cloudfront.this_cloudfront_distribution_hosted_zone_id
       }
     },
   ]
